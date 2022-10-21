ec78856fdda895a1225ca5d6c8fc82a7a3a97264
EC2 Default User <ec2-user@ip-172-31-9-221.us-east-2.compute.internal>
2022-10-21 02:23:48 +0000

    week 6 thurs

--- assignments/week-6-thurs/train.py
+++ assignments/week-6-thurs/train.py
@@ -1,3 +1,4 @@
+
 from dataset import OxfordPets
 from tensorflow import keras
 from unet import get_model
@@ -6,12 +7,16 @@ import os
 
 import mlflow
 import mlflow.keras
+# Enable auto-logging to MLflow to capture TensorBoard metrics.
+mlflow.tensorflow.autolog()
+from tensorflow.keras.callbacks import ModelCheckpoint
 
-# Add mlflow logging
+#tensorflow.keras.enable_eager_execution()
+#tensorflow.keras.executing_eagerly() # True
 
+input_dir = "/home/ec2-user/MLOPs/assignments/week-6-thurs/data/"
+target_dir = "/home/ec2-user/MLOPs/assignments/week-6-thurs/data/annotations/trimaps/"
 
-input_dir =
-target_dir =
 img_size = (160, 160)
 num_classes = 3
 batch_size = 32
@@ -46,10 +51,24 @@ val_gen = OxfordPets(batch_size, img_size, val_input_img_paths, val_target_img_p
 model.compile(optimizer="rmsprop", loss="sparse_categorical_crossentropy")
 
 # Set the keras callback using ModelCheckpoint to save the best results of the model
-callbacks =
+
+checkpoint_filepath = "ckpts"
+callbacks = ModelCheckpoint(
+    filepath=checkpoint_filepath,
+    save_weights_only=True,
+    monitor='val_accuracy',
+    mode='max',
+    save_best_only=True)
 
 # Train the model, doing validation at the end of each epoch.
 # Make sure to include the calllback for mlflow
-epochs = 15
-model.fit(train_gen, epochs=epochs, validation_data=val_gen)
-model.save('./segmentation/')
+epochs = 3
+
+
+
+with mlflow.start_run():
+    model.fit(train_gen, epochs=epochs, validation_data=val_gen, callbacks=[callbacks])
+    model.save('./segmentation/')
+
+#    print_auto_logged_info(mlflow.get_run(run_id=run.info.run_id))
+
